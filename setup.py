7f6acd74ac4bf25708581e05c28eac21d2c1f0ab
roodletoof <68161791+roodletoof@users.noreply.github.com>
2021-08-05 21:05:20 +0200

    updated to right github ling

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description="A small tweening module",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/clement-bonnet/medium-first-package",
+    url="https://github.com/roodletoof/tween",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
